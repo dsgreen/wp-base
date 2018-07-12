43b1c358aa17db3cb8d6f00f6b4f1618289a6fe2
Doug Green <doug@dsgreen.com>
2018-07-12 15:38:13 -0400

    Move thumbnail above post title

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,6 +10,8 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php _s_post_thumbnail(); ?>
+
 	<header class="entry-header">
 		<?php
 		if ( is_singular() ) :
@@ -28,8 +30,6 @@
 		<?php
 		endif; ?>
 	</header><!-- .entry-header -->
-
-	<?php _s_post_thumbnail(); ?>
 
 	<div class="entry-content">
 		<?php
