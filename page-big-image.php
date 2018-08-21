4e6994a24cdcb2147d7ebff2fccec3e9be908286
Doug Green <doug@dsgreen.com>
2018-08-21 15:02:02 -0400

    Make image header an include

--- page-big-image.php
+++ page-big-image.php
@@ -11,23 +11,7 @@ get_header();
 ?>
 
 <?php while ( have_posts() ) : the_post(); ?>
-<style>
-.hero--sub {
-  background-image: url('<?php
-    if ( has_post_thumbnail() ) {
-      the_post_thumbnail_url('full');
-    } ?>');
-}
-</style>
-<section class="hero hero--sub" id="hero">
-  <div class="container">
-	  <?php the_title( '<h1>', '</h1>' ); ?>
-	  <?php if ($lead = get_post_meta(get_the_ID(), 'lead', true)): ?>
-      <div class="lead"><h2><?php echo esc_html($lead); ?></h2></div>
-	  <?php endif; ?>
-  </div>
-  <div class="color-overlay"></div>
-</section>
+<?php get_template_part( 'template-parts/image-header' ); ?>
 <section class="site-content container" id="content">
   <div class="row">
       <main class="col-10 col site-main" id="main" role="main">
