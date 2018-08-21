fd324adcc55616dde63f38e38e331be0a53d7121
Doug Green <doug@dsgreen.com>
2018-08-20 20:53:21 -0400

    Add footer widget areas to template

--- footer.php
+++ footer.php
@@ -19,6 +19,30 @@
 	</section><!--/#content-->
 <?php } ?>
 
+<?php if ( is_active_sidebar( 'footer-1' ) || is_active_sidebar( 'footer-2' ) || is_active_sidebar( 'footer-3' ) ) : ?>
+<section class="pre-footer">
+  <div class="container">
+    <div class="row">
+      <?php if ( is_active_sidebar( 'footer-1' ) ) { ?>
+      <div class="col-4 col">
+	      <?php dynamic_sidebar( 'footer-1' ); ?>
+      </div>
+      <?php } ?>
+      <?php if ( is_active_sidebar( 'footer-2' ) ) { ?>
+      <div class="col-4 col">
+	      <?php dynamic_sidebar( 'footer-2' ); ?>
+      </div>
+      <?php } ?>
+      <?php if ( is_active_sidebar( 'footer-3' ) ) { ?>
+      <div class="col-4 col">
+	      <?php dynamic_sidebar( 'footer-3' ); ?>
+      </div>
+      <?php } ?>
+    </div>
+  </div>
+</section>
+<?php endif; ?>
+
 <footer class="site-footer" role="contentinfo">
 	<div class="container site-info">
     <div class="row">
